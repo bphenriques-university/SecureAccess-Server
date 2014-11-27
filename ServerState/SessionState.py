773a525ec4dc2e57414c3655f4955c72118888d5
tiagolb <tiagoluisbrito@gmail.com>
2014-11-27 17:57:05 +0000

    fixes

--- ServerState/SessionState.py
+++ ServerState/SessionState.py
@@ -10,6 +10,7 @@ class Session():
 		self._client_sock = client_sock		
 		#The initial state is Start
 		self._session_state = StartState()
+		self._is_interrupted = False
 	
 	def getClientSocket(self):
 		return self._client_sock
@@ -20,9 +21,15 @@ class Session():
 	def setNextState(self, new_state):
 		self._session_state = new_state
 
+	def interrupt(self):
+		self._is_interrupted = True
+
+	def log(self, msg):
+		print ("[" + str(self._client_id) + "] - " + msg)
+
 	def start(self):
 		try:
-			while True:
+			while not self._is_interrupted:
 				self._session_state.handle(self)
 				#raw_input("Press any key to continue...")
 		except Exception as e:
@@ -37,10 +44,46 @@ class Session():
 class StartState(AbstractSessionState):
 	def __init__(self):
 		AbstractSessionState.__init__(self)
+		self.__COMMAND = "CONN"
+		self.__challenge = None
+
+	def parse_data(self, data):
+		# arguments are separated by '#'
+		if len(data) == 0:
+			return False
+			
+		if len(data) != 0: 
+			data_to_parse = str(data)
+			data_to_parse = data_to_parse.split("#")
+			command = data_to_parse[0]
+			if command == self.__COMMAND and len(data_to_parse) == 2:
+				self.__challenge = int(data_to_parse[1])
+			else:
+				return False
+
+		return True
 
 	def handle(self, session_manager):
-		print ("[" + str(session_manager.getClientId()) + "] - Start State: Waiting for CONNECT MESSAGE")
+		session_manager.log("Start State: Waiting for CONNECT MESSAGE")
+		c_socket = session_manager.getClientSocket();
+
+		data = c_socket.recv(1024)
+		session_manager.log("Start State: Received: " + str(data))
+	
+		if not self.parse_data(data):
+			raise Exception("Bad input from client, aborting session")		
+
+
+		session_manager.log("Start State: Challenge number is: " + str(self.__challenge))
+		challenge = self.__challenge
+		challenge = challenge - 1
+
+		key = "WWW.OSOCRATESAINDAESTAPRESO.COM"
+
+		response = "CONN_RESPONSE#" + key + "#" + str(challenge)
 		
+		c_socket.send(response)
+			
 		#work to be done here
 		session_manager.setNextState(EchoReplyState())
 
@@ -53,27 +96,10 @@ class EchoReplyState(AbstractSessionState):
 		AbstractSessionState.__init__(self)
 
 	def handle(self, session_manager):
-		print ("[" + str(session_manager.getClientId()) + "] - Start State: Waiting for CONNECT MESSAGE")		
+		#session_manager.log("EchoReplyState: Pinging client...")		
 		c_socket = session_manager.getClientSocket();
 		session_manager.setNextState(EchoReplyState())
-			
+
 	
-	def parse_data(self, data):
-		# arguments are separated by '#'
-		if len(data) == 0:
-			return False
-			
-		if len(data) != 0: 
-			data_to_parse = str(data)
-			data_to_parse = data_to_parse.split("#")
-			command = data_to_parse[0]
-			if command == self.__expectedCommand and len(data_to_parse) == 4:
-				self.__base = long(data_to_parse[1])
-				self.__prime = long(data_to_parse[2])
-				self.__yDevice = long(data_to_parse[3])
-			else:
-				return False
-		
-		return True
 #data = c_socket.recv(1024)
 #c_socket.send(msg_to_send)
