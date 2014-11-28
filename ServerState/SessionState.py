c97b34f1a010bb14272abef6e79eb0698a7508be
tiagolb <tiagoluisbrito@gmail.com>
2014-11-28 10:33:20 +0000

    ping-echo with nounces

--- ServerState/SessionState.py
+++ ServerState/SessionState.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python2
 # -*- coding: UTF-8 -*-
 
+import sys, time
+from random import *
 from AbstractSessionState import AbstractSessionState
 
 #State machine MANAGER
@@ -47,7 +49,7 @@ class StartState(AbstractSessionState):
 		self.__COMMAND = "CONN"
 		self.__challenge = None
 
-	def parse_data(self, data):
+	def parse_connect_request(self, data):
 		# arguments are separated by '#'
 		if len(data) == 0:
 			return False
@@ -63,41 +65,92 @@ class StartState(AbstractSessionState):
 
 		return True
 
+	def generate_session_key(self):
+		return "ab1bn32b31jknmd102ui4bnfsanocm9123"
+
 	def handle(self, session_manager):
-		session_manager.log("Start State: Waiting for CONNECT MESSAGE")
+		session_manager.log("Waiting for CONNECT MESSAGE")
 		c_socket = session_manager.getClientSocket();
 
+
+		# RECEIVE CONNECTION REQUEST
 		data = c_socket.recv(1024)
-		session_manager.log("Start State: Received: " + str(data))
+		session_manager.log("[DEVICE]: " + str(data))
 	
-		if not self.parse_data(data):
+		if not self.parse_connect_request(data):
 			raise Exception("Bad input from client, aborting session")		
 
+		# ANSWERING CLIENT REQUEST WITH HIS CHALLENGE - 1 AND A NEW CHALLENGE FOR HIM
 
-		session_manager.log("Start State: Challenge number is: " + str(self.__challenge))
-		challenge = self.__challenge
-		challenge = challenge - 1
+		key = self.generate_session_key()
 
-		key = "WWW.OSOCRATESAINDAESTAPRESO.COM"
+		challenge_client = randint(0, 2147483647)
+		response = "CONN_RESPONSE#" + key + "#" + str(self.__challenge-1) + "#" + str(challenge_client)
 
-		response = "CONN_RESPONSE#" + key + "#" + str(challenge)
-		
+		session_manager.log("[SERVER]: " + str(response))		
 		c_socket.send(response)
+
+
+		# RECEIVE CLIENT RESPONSE TO HIS CHALLENGE
+		data = int(c_socket.recv(1024))
+		session_manager.log("[Device]: " + str(data))
+		if(data != (challenge_client - 1)):
+			raise Exception("Client failed authentication challenge, aborting session")
 			
+		session_manager.log("[LOG]: AUTHENTICATED")
+		
 		#work to be done here
 		session_manager.setNextState(EchoReplyState())
 
 #
-#	Server waits for new connections
+#	Ping-request - Ping-reply State
 #
 
 class EchoReplyState(AbstractSessionState):
 	def __init__(self):
 		AbstractSessionState.__init__(self)
+		self.__TIMER_SECONDS = 5
+		self.__RESPONSE_COMMAND = "PING_RES"
+		self.__challenge = None
+
+	def parse_ping_response(self, data):
+		# arguments are separated by '#'
+		if len(data) == 0:
+			return False
+			
+		if len(data) != 0: 
+			data_to_parse = str(data)
+			data_to_parse = data_to_parse.split("#")
+			command = data_to_parse[0]
+			if command == self.__RESPONSE_COMMAND and len(data_to_parse) == 2:
+				self.__challenge = int(data_to_parse[1])
+			else:
+				return False
+
+		return True
 
 	def handle(self, session_manager):
 		#session_manager.log("EchoReplyState: Pinging client...")		
 		c_socket = session_manager.getClientSocket();
+
+		#SENDING PING_REQUEST
+		challenge_ping = randint(0, 2147483647)		
+		ping_request = "PING_REQ#" + str(challenge_ping) 
+		session_manager.log("[SERVER]: " + str(ping_request))
+		c_socket.send(ping_request)
+
+		#WAITING FOR PING REPLY
+		data = c_socket.recv(1024)
+		session_manager.log("[DEVICE]: " + str(data))
+		if not self.parse_ping_response(data):
+			raise Exception("Client failed ping response, aborting session")
+		
+		if not self.__challenge == challenge_ping - 1:
+			raise Exception("Client failed ping response, aborting session") 
+		
+
+		#ping device again in TIMER_SECONDS again
+		time.sleep(self.__TIMER_SECONDS)
 		session_manager.setNextState(EchoReplyState())
 
 	
